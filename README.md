845cc3bb08f0e746f3b291224ff7b5ba88e9dfd2
unknown <pvajay.thota@gmail.com>
2021-05-21 20:15:19 +0530

    automated login with unhandled exceptions. Bhagwan k bharose

--- README.md
+++ README.md
@@ -1,2 +1,9 @@
 # CoWin
+
 attempt to auto register for vaccination in the CoWin vaccine portal.
+
+these below pre requisites should be run before running using this module.
+
+```ps1
+Install-Module Selenium -Scope CurrentUser -Confirm:$false -Verbose
+```
--- browser-automation.ps1
+++ browser-automation.ps1
@@ -1,2 +1,45 @@
-install-modulue selenium 
 # browser automation using selenium
+
+# Import-Module Selenium 
+#USER INPUTS
+$MobileNumber = '9971798371'
+
+# Predefined variables
+# $CoWinSite = "https://www.cowin.gov.in/"
+$CowinLoginURL = "https://selfregistration.cowin.gov.in/"
+
+
+$Chrome = Start-SeChrome -StartURL $CowinLoginURL -WebDriverDirectory .\chromedriver_win32\ -Quiet
+
+$Global:Chrome = $Chrome
+
+function EnterMobileNumber {
+    [cmdletbinding()]
+    param($MobileNumber)
+    $id = "mat-input-0"
+    $ele = $Global:Chrome.FindElementById($id)
+    $ele.Click()
+    $ele.SendKeys($MobileNumber)
+    $getOtpBtnclsNM = 'covid-button-desktop'
+    $Global:Chrome.FindElementsByClassName($getOtpBtnclsNM).click()
+}
+
+function EnterOTP {
+    [cmdletbinding()]
+    param($otp)
+
+    $ele = $Global:Chrome.FindElementByCssSelector('#mat-input-1')
+    $ele.click()
+    $ele.sendkeys($otp)
+    $veryfyOtpbtn = '#main-content > app-login > ion-content > div > ion-grid > ion-row > ion-col > ion-grid > ion-row > ion-col > ion-grid > form > ion-row > ion-col:nth-child(3) > div > ion-button'
+    $ele = $Global:Chrome.FindElementsByCssSelector($veryfyOtpbtn)
+    $ele.click()
+
+}
+
+EnterMobileNumber $MobileNumber
+# $ele.sendkeys([OpenQA.Selenium.Keys]::Enter)
+
+$otp = Read-Host Enter OTP Received
+
+EnterOTP $otp
